66147fdd863e3ec53debb4a26919ba4b119eeae3
Sam Wyness <samwyness22@gmail.com>
2018-06-27 00:53:47 +1000

    Added api path to theme env vars.

--- env.config.js
+++ env.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     theme_name: 'tk_react',
     proxy_target: 'http://tk-react.local', // Point to where your wordpress install is served i.e 'http://localhost:8080'
+    api: 'http://tk-react.local/wp-json/tkr/v1/',
     paths: {
         base: path.resolve(__dirname),
         build: path.resolve(__dirname, 'build'),
