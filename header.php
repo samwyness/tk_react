2583b2fd039005282b37f645dac15fbd6ba290e4
Sam Wyness <samwyness22@gmail.com>
2018-06-27 00:24:27 +1000

    WIP
    Added manifest file (PWA wip)

--- header.php
+++ header.php
@@ -12,7 +12,10 @@
     <head>
     	<meta charset="<?php bloginfo( 'charset' ); ?>">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#ffffff">
 
+        <link rel="manifest" href="./manifest.json">
+        
         <?php wp_head(); ?>
 	</head>
 
