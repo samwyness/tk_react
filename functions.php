1f2074fa43b2ea2353bc2d14ca171a9f5c8b71ca
Sam Wyness <samwyness22@gmail.com>
2018-07-01 03:21:49 +1000

    Added post thumbnail response in wp api.

--- functions.php
+++ functions.php
@@ -81,3 +81,29 @@ add_action( 'wp_head', function() {
 add_filter( 'excerpt_length', function( $length ) {
 	return 24;
 }, 999 );
+
+
+/*
+ *
+ * Add post feature image to wp rest
+ *
+ */
+add_action('rest_api_init', 'register_rest_images' );
+function register_rest_images() {
+    register_rest_field( array('post'),
+        'fimg_url',
+        array(
+            'get_callback'    => 'get_rest_featured_image',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
+
+function get_rest_featured_image( $object, $field_name, $request ) {
+    if( $object['featured_media'] ){
+        $img = wp_get_attachment_image_src( $object['featured_media'], 'app-thumb' );
+        return $img[0];
+    }
+    return false;
+}
