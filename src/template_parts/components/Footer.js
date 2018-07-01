13d056730c04b4ad48f9d936a147a1b7c2172f86
Sam Wyness <samwyness22@gmail.com>
2018-07-01 20:46:59 +1000

    Updated footer styles.

--- src/template_parts/components/Footer.js
+++ src/template_parts/components/Footer.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Footer = () => (
-    <footer className="tk-footer"></footer>
+    <footer className="tk-footer tk-splash tk-dark">
+        <h1>tk_react</h1>
+    </footer>
 );
 
 export default Footer;
