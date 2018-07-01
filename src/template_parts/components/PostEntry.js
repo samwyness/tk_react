1f05888ef173c18ee6388f02d36f0bc1d68af58d
Sam Wyness <samwyness22@gmail.com>
2018-07-01 20:49:15 +1000

    Updated post entry props and added replace method for excerpt brackets.

--- src/template_parts/components/PostEntry.js
+++ src/template_parts/components/PostEntry.js
@@ -2,33 +2,31 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom'
 
 export default class PostEntry extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            image: this.props.image,
-            title: this.props.title,
-            content: this.props.content,
-            date: this.props.date
-        }
+
+    replaceExcerptBrackets( excerpt ) {
+        return excerpt.replace( /\[&hellip;]/g, '...' );
     }
 
     createPostContentMarkup() {
-        return {__html: this.state.content};
+        return {__html: this.replaceExcerptBrackets( this.props.excerpt.rendered )};
     }
 
     render() {
         return (
             <div className="tk-post">
                 <div className="tk-post-title">
-                    <h2>{this.state.title}</h2>
+                    <h2>{this.props.title.rendered}</h2>
                 </div>
 
-                <div className="tk-post-date">{new Date(this.state.date).toLocaleString()}</div>
+                <div className="tk-post-date">{new Date(this.props.date).toLocaleString()}</div>
 
                 <div className="tk-post-content" dangerouslySetInnerHTML={this.createPostContentMarkup()}></div>
 
-                {/* <NavLink className="tk-btn" to={this.props.link_text}>{this.props.link_text}</NavLink> */}
+                <NavLink className="tk-btn" to={this.props.link_to.replace( __TK__.urls.base, '' )}>
+                    {this.props.link_text}
+                </NavLink>
             </div>
         );
     }
+
 };
