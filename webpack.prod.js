48ea17c77799155772e87e6de8b78c1a6a5172ba
Sam Wyness <samwyness22@gmail.com>
2018-07-05 00:05:30 +1000

    Clean up webpack config.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,8 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = merge(common, {
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
-        }),
-        new UglifyJSPlugin({
-            sourceMap: true
-        })
-    ]
+    mode: 'production'
 });
