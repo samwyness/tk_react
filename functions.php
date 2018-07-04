108f9576488fcc5d430cc034314b0e24f728e563
Sam Wyness <samwyness22@gmail.com>
2018-07-05 00:04:52 +1000

    Removed wp emoji scripts.

--- functions.php
+++ functions.php
@@ -33,9 +33,7 @@ add_action( 'after_setup_theme', 'tk_react_setup' );
  */
 add_action( 'wp_enqueue_scripts', function() {
 	// Theme css files.
-	// wp_enqueue_style( 'tk_react-styles', get_stylesheet_uri(), '1.0', true );
 	wp_enqueue_style( 'tk_react-styles', get_theme_file_uri( '/src/css/critical.css' ), '1.0', true );
-	// wp_enqueue_style( 'tk_react-styles', get_theme_file_uri( '/src/css/critical.css' ), '1.0', true );
 
 	// Theme js files.
 	wp_enqueue_script( 'tk_react-scripts', get_theme_file_uri( '/tkr.bundle.js' ), array(), '1.0', true );
@@ -76,6 +74,15 @@ add_action( 'wp_head', function() {
   	echo "<script> window.{$var} = {$data}; </script>\n";
 
 }, 10 );
+
+
+/*
+ *
+ * Remove wp emoji scripts and styles
+ *
+ */
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
 
 
 /*
