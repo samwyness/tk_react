ed1166a97c991239b93347551f32bd3f87949ae3
Sam Wyness <samwyness22@gmail.com>
2018-10-25 20:55:29 +1000

    Fixed page content not updating on url change.

--- src/template_parts/Page.js
+++ src/template_parts/Page.js
@@ -3,66 +3,92 @@ import React, { Component } from 'react';
 import Splash from './components/Splash';
 
 export default class Page extends Component {
-  constructor( props ) {
-      super( props )
-      this.state = {
-          page_data: false
-      }
-  }
+    constructor( props ) {
+        super( props )
+        this.state = {
+            page_data: false,
+            loading: false
+        }
+    }
 
-  componentDidMount() {
-      // TODO: Move this fetch to index.js and handle post slug dynamically
-      //       then we can pass page data down as props.
-      let page_slug = this.props.location.pathname;
+    componentDidMount() {
+        return this.fetchPageData();
+    }
 
-      fetch( __TK__.urls.wp_api + '/pages?slug=' + page_slug )
-      .then( response => response.json() )
-      .then( json => {
-          this.setState( {
-              page_data: json[0] || false
-          } );
-      } )
-      .catch( error => { console.log( error ) } );
-  }
+    shouldComponentUpdate( nextProps, nextState ) {
+        if (!nextState.page_data) {
+            this.setState( {
+                page_data: {
+                    title: { rendered: 'Page Template' }
+                }
+            } );
 
-  shouldComponentUpdate( nextProps, nextState ) {
-      if (!nextState.page_data) {
-          this.setState( {
-              page_data: {
-                  title: { rendered: 'Page Template' }
-              }
-          } );
+            return false;
+        }
 
-          return false;
-      }
+        return true;
+    }
 
-      return true;
-  }
+    componentDidUpdate( prevProps, prevState ) {
+        let prev_slug = prevProps.match.params.slug;
+        let current_slug = this.props.match.params.slug;
 
-  createPageContentMarkup() {
-      let content = (this.state.page_data.content) ? this.state.page_data.content.rendered : '';
-      return {__html: content};
-  }
+        if (prev_slug !== current_slug) {
+            return this.fetchPageData();
+        }
 
-  render() {
-      let page_data = this.state.page_data || false;
-      let page_title = (page_data) ? page_data.title.rendered : false;
+        return false;
+    }
 
-      return (
-          <div className="tk-content">
+    fetchPageData() {
+        let page_slug = this.props.match.params.slug;
 
-              <Splash
-                  bg_class="tk-hot"
-                  title={page_title}
-              />
+        this.setState( {
+            loading: true
+        } );
 
-              <div className="container">
+        fetch( __TK__.urls.wp_api + '/pages?slug=' + page_slug )
+        .then( response => response.json() )
+        .then( json => {
+            this.setState( {
+                page_data: json[0],
+                loading: false
+            } );
+        } )
+        .catch( error => { console.log( error ) } );
+    }
 
-                  <div dangerouslySetInnerHTML={this.createPageContentMarkup()}></div>
+    createPageContentMarkup() {
+        let content = (this.state.page_data.content) ? this.state.page_data.content.rendered : '';
+        return {__html: content};
+    }
 
-              </div>
+    render() {
+        let page_data = this.state.page_data || false;
+        let page_title = (page_data) ? page_data.title.rendered : false;
+        let loading = (this.state.loading) ? 'Loading...' : false;
 
-          </div>
-      );
-  }
+        if (loading) {
+            page_title = loading;
+        }
+
+        console.log('%cRender: ', 'color: green');
+
+        return (
+            <div className="tk-content">
+
+                <Splash
+                    bg_class="tk-hot"
+                    title={page_title}
+                />
+
+                <div className="container">
+
+                    <div dangerouslySetInnerHTML={this.createPageContentMarkup()}></div>
+
+                </div>
+
+            </div>
+        );
+    }
 }
