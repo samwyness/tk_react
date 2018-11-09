75785782cbbc6c2484b2d6306d949f90da3365f8
Sam Wyness <samwyness22@gmail.com>
2018-11-09 22:21:05 +1000

    Clean up splash component.

--- src/template_parts/components/Splash.js
+++ src/template_parts/components/Splash.js
@@ -3,17 +3,16 @@ import { NavLink } from 'react-router-dom'
 
 export default class Splash extends Component {
     render() {
-        let button;
-        if (this.props.link_to) {
-            button = <NavLink className="tk-btn" to={this.props.link_to}>{this.props.link_text}</NavLink>;
-        }
-
         return (
-            <section className={'tk-splash ' + this.props.bg_class}>
-                <h1>{this.props.title}</h1>
-                <br></br>
-                <p>{this.props.text}</p>
-                { button }
+            <section className={ 'tk-splash ' + this.props.bg_class }>
+                <div className="container">
+                    <h1>{ this.props.title }</h1>
+                    <br></br>
+                    <p>{ this.props.text }</p>
+                    { ( this.props.link_to ) &&
+                        <NavLink className="tk-btn" to={this.props.link_to}>{this.props.link_text}</NavLink>
+                    }
+                </div>
             </section>
         );
     }
