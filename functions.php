03aa94be1538f24d0aecddfd3d200634a23e7071
Sam Wyness <samwyness22@gmail.com>
2018-12-20 18:20:26 +1000

    Added theme support for SVG uploads.

--- functions.php
+++ functions.php
@@ -133,6 +133,17 @@ add_filter( 'excerpt_length', function( $length ) {
 
 /*
  *
+ * Enable SVG support
+ *
+ */
+add_filter( 'upload_mimes', 'cc_mime_types' );
+function cc_mime_types( $mimes ) {
+  	$mimes['svg'] = 'image/svg+xml';
+  	return $mimes;
+}
+
+/*
+ *
  * Theme includes
  *
  */
