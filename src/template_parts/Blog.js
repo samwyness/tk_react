5a0b3715742262dca42e0d3a68c120d17cffe085
Sam Wyness <samwyness22@gmail.com>
2018-12-21 12:41:40 +1000

    Updated Blog Template.

--- src/template_parts/Blog.js
+++ src/template_parts/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
 import SectionHero from './components/SectionHero';
-import SectionPosts from './components/SectionPosts';
+import SectionCustom from './components/SectionCustom';
+import ContentPosts from './components/ContentPosts';
 
 export default class Blog extends Component {
     render() {
@@ -9,23 +10,15 @@ export default class Blog extends Component {
             <div className="tk-content">
 
                 <SectionHero
-                    bg_class="tk-hot"
+                    hero_class="tk-hot"
                     title="Blog Template"
-                    text="mmmm fast eh."
-                    link_to="/"
-                    link_text="Back Home"
                 />
 
-                <section>
-                    <div className="container">
-
-                        <h1 style={ { textTransform: 'uppercase' } }>Recent <span className="tk-hot-title">Posts</span></h1>
-                        <span className="tk-title-underline"></span>
-
-                        <SectionPosts/>
-
-                    </div>
-                </section>
+                <SectionCustom
+                    title="Recent Posts"
+                >
+                    <ContentPosts/>
+                </SectionCustom>
 
             </div>
         );
