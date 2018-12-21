dc5fb0a556a0e82aec2878e55ac5dbf67f2fd180
Sam Wyness <samwyness22@gmail.com>
2018-12-21 12:38:55 +1000

    Removed unnecessary components.

--- src/template_parts/Image.js
+++ src/template_parts/Image.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import SectionHero from './components/SectionHero';
-import SectionPosts from './components/SectionPosts';
 
 export default class Image extends Component {
     render() {
@@ -9,20 +8,11 @@ export default class Image extends Component {
             <div className="tk-content">
 
                 <SectionHero
-                    bg_class="tk-hot"
+                    hero_class="tk-hot"
                     title="Image Template"
                     link_to="/"
                     link_text="Back Home"
                 />
-
-                <section>
-                    <div className="container">
-
-                        <h1 style={ { textTransform: 'uppercase' } }>Image</h1>
-                        <span className="tk-title-underline"></span>
-
-                    </div>
-                </section>
 
             </div>
         );
--- src/template_parts/Search.js
+++ src/template_parts/Search.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
 
 import SectionHero from './components/SectionHero';
-import SectionPosts from './components/SectionPosts';
 
-export default class Search extends Component {
+export default class Image extends Component {
     render() {
         return (
             <div className="tk-content">
 
                 <SectionHero
-                    bg_class="tk-hot"
+                    hero_class="tk-hot"
                     title="Search Template"
                     link_to="/"
                     link_text="Back Home"
                 />
-
-                <section>
-                    <div className="container">
-
-                        <h1 style={ { textTransform: 'uppercase' } }>Search</h1>
-                        <span className="tk-title-underline"></span>
-
-                    </div>
-                </section>
 
             </div>
         );
