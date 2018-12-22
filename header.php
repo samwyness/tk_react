5cd9d1b9c21032d04ca3518df8bf95da75d42f3e
Sam Wyness <samwyness22@gmail.com>
2018-12-22 20:26:10 +1000

    Added meta description; Added profile and pingback links

--- header.php
+++ header.php
@@ -10,9 +10,15 @@
 <html <?php language_attributes(); ?> dir="ltr">
 
     <head>
-    	<meta charset="<?php bloginfo( 'charset' ); ?>">
-    	<meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
         <meta name="theme-color" content="#ffffff">
+        <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
         <!-- <link rel="manifest" href="./manifest.json"> -->
 
