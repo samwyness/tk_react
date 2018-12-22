44dea4654f5b3a94de831ef45cf4accb69964640
Sam Wyness <samwyness22@gmail.com>
2018-12-22 20:30:07 +1000

    Added mobile slide navigation.

--- src/template_parts/components/Header.js
+++ src/template_parts/components/Header.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-
 import { Link } from 'react-router-dom'
-
 import Menu from './Menu';
 
 export default class Header extends Component {
-
     constructor( props ) {
         super( props )
         this.state = {
-            classes: 'tk-top-nav'
+            classes: 'tk-top-nav',
+            toggle: false
         }
         this.top_nav = React.createRef();
+        this.slide_nav = React.createRef();
         this.onScroll = this.onScroll.bind( this );
+        this.handleClick = this.handleClick.bind( this );
     }
 
     componentDidMount() {
@@ -21,6 +21,14 @@ export default class Header extends Component {
 
     componentWillUnmount() {
         window.removeEventListener( 'scroll', this.onScroll );
+    }
+
+    componentWillReceiveProps( nextProps ) {
+        if ( this.props.location !== nextProps.location ) {
+            this.setState( {
+                toggle: false
+            } );
+        }
     }
 
     onScroll() {
@@ -34,8 +42,17 @@ export default class Header extends Component {
         }
     }
 
+    handleClick() {
+        this.setState( {
+            toggle: !this.state.toggle
+        } );
+    }
+
     render() {
         let logo_src = ( __TK__.settings.site_logo ) ? __TK__.settings.site_logo : false;
+        let slide_nav_toggle = this.state.toggle || false;
+        let slide_nav_classes = [ 'tk-slide-nav', ( this.state.toggle ? 'open' : '' ) ];
+        let menu_button_text = ( this.state.toggle ) ? 'Close' : 'Menu';
 
         return (
             <header className={ this.state.classes } ref={ this.top_nav }>
@@ -59,6 +76,16 @@ export default class Header extends Component {
 
                     <Menu menu_location="top-nav-menu"/>
 
+                    <a className="tk-btn tk-menu-btn align-right" onClick={ this.handleClick }>{ menu_button_text }</a>
+
+                </div>
+
+                <div className={ slide_nav_classes.join( ' ' ) }>
+                    <div className="container">
+
+                        <Menu menu_location="top-nav-menu"/>
+
+                    </div>
                 </div>
             </header>
         );
