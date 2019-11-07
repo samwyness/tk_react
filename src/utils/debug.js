cfee3b9a4fd18c0e8b58253a98c2538b0f714411
Sam Wyness <samwyness22@gmail.com>
2019-11-07 17:10:08 +1000

    Added frontend debug method.

---
+++ src/utils/debug.js
@@ -0,0 +1,14 @@
+const isProd = (process.envNODE_ENV === 'production');
+
+export const debug = (message, color) => {
+	if (!isProd) {
+		if (color) {
+			console.groupCollapsed('%c [TKR-DEBUG]', `color: ${color}`, message);
+		} else {
+			console.groupCollapsed('[TKR-DEBUG]', message);
+		}
+		
+		console.trace();
+		console.groupEnd();
+	}
+};
