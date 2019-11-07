2564c1920f93732d17c165de8e5938f1191def0c
Sam Wyness <samwyness22@gmail.com>
2019-11-07 17:11:49 +1000

    Added button and button link components.

---
+++ src/components/ButtonLink.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { A, usePath, setLinkProps } from 'hookrouter';
+
+import Button from 'components/Button';
+
+const ButtonLink = (props) => {
+	const currentPath = usePath();
+	let button_class = 'tkr-btn-link';
+
+	// Add an active class to the button if the href matches the current page 
+	if (props.href === currentPath) {
+		button_class = `${button_class} active`;
+	}
+
+	return (
+		<A { ...setLinkProps(props) }>
+			<Button className={ button_class }>
+				{ props.children }
+			</Button>
+		</A>
+	);
+};
+
+export default ButtonLink;
