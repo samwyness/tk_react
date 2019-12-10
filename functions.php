0fd2b8aa37b3472ce10ab6d52252a9b6da4d5f30
Sam Wyness <samwyness22@gmail.com>
2019-12-10 22:28:39 +1000

    Added theme support for html5 markup.

--- functions.php
+++ functions.php
@@ -59,6 +59,23 @@ function tk_react_theme_setup() {
 	// Add support for full and wide align images.
 	add_theme_support( 'align-wide' );
 
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'script',
+			'style',
+		)
+	);
+}
 		'main-menu'		=> __( 'Main Menu', 'tk_react' ),
 	) );
 }
