7616bab6c6381ec4e3f271d4db72539f22e32c39
Sam Wyness <samwyness22@gmail.com>
2019-12-10 22:41:48 +1000

    Updated header and footer components.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,70 +10,58 @@ const site_title = __tkr__.settings.meta.title;
 
 const Footer = () => {
 	return(
-		<footer className="tkr-footer pt-5">
-			<div className="container">
-				<div className="row">
+		<footer className="tkr-footer">
+			<div className="tkr-footer-top py-5">
+				<div className="container">
+					<div className="row">
 
-					<div className="col-md-4">
-						<A href="/" className="tkr-site-branding">
-							{ site_logo &&
-							<span className="tkr-site-logo">
-								<img src={ site_logo } />
-							</span>
-							}
+						<div className="col-md-4">
+							<A href="/" className="tkr-site-branding">
+								{ site_logo &&
+									<span className="tkr-site-logo">
+										<img src={ site_logo } />
+									</span>
+								}
 
-							{ !site_logo && site_title &&
-							<span className="tkr-site-title">
-								<strong>{ site_title }</strong>
-							</span>
-							}			
-						</A>
+								{ !site_logo && site_title &&
+									<span className="tkr-site-title">
+										<strong>{ site_title }</strong>
+									</span>
+								}			
+							</A>
 						
-						<ul className="tkr-social-links mt-3 list-style-none list-horizontal">
-							<li><Icon icon="facebook"/></li>
-							<li><Icon icon="instagram"/></li>
-							<li><Icon icon="linkedin"/></li>
-							<li><Icon icon="youtube"/></li>
-							<li><Icon icon="gmail"/></li>
-						</ul>
-					</div>
-					
-					<div className="col-md-8">
-						<div className="row">
-							<ul className="col-md-3 my-3 list-style-none">
-								<li><A href="/#">Terms</A></li>
-								<li><A href="/#">Privacy Policy</A></li>
-								<li><A href="/#">Terms</A></li>
-								<li><A href="/#">Privacy Policy</A></li>
-							</ul>
-
-							<ul className="col-md-3 my-3 list-style-none">
-								<li><A href="/#">Terms</A></li>
-								<li><A href="/#">Privacy Policy</A></li>
-								<li><A href="/#">Terms</A></li>
-								<li><A href="/#">Privacy Policy</A></li>
-							</ul>
-
-							<ul className="col-md-3 my-3 list-style-none">
-								<li><A href="/#">Terms</A></li>
-								<li><A href="/#">Privacy Policy</A></li>
-								<li><A href="/#">Terms</A></li>
-								<li><A href="/#">Privacy Policy</A></li>
-							</ul>
-
-							<ul className="col-md-3 my-3 list-style-none">
-								<li><A href="/#">Terms</A></li>
-								<li><A href="/#">Privacy Policy</A></li>
-								<li><A href="/#">Terms</A></li>
-								<li><A href="/#">Privacy Policy</A></li>
+							<ul className="tkr-social-links mt-3 list-style-none list-horizontal">
+								<li><Icon icon="facebook"/></li>
+								<li><Icon icon="instagram"/></li>
+								<li><Icon icon="linkedin"/></li>
+								<li><Icon icon="youtube"/></li>
+								<li><Icon icon="gmail"/></li>
 							</ul>
 						</div>
-					</div>
+					
+						<div className="tkr-footer-links col-md-8">
+							<div className="row">
 
+								<div className="col-md-3">
+									<div className="tkr-footer-links-title">
+										<h4 className="mt-0">Discover</h4>
+									</div>
+
+									<ul className="px-0 list-style-none">
+										<li className="mb-2"><A href="/#">Terms</A></li>
+										<li className="mb-2"><A href="/#">Privacy Policy</A></li>
+										<li className="mb-2"><A href="/#">Terms</A></li>
+										<li className="mb-2"><A href="/#">Privacy Policy</A></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+
+					</div>
 				</div>
 			</div>
 			
-			<div className="tkr-footer-bottom mt-5">
+			<div className="tkr-footer-bottom">
 				<div className="container justify-content-center">
 					
 					<span>© { new Date().getFullYear() } Sam Wyness</span>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,85 +1,22 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import { A } from 'hookrouter';
 
 // Store
 import { useMainMenu } from 'store/actions/appActions';
 
 // Components
-import Button from 'components/Button';
 import ButtonLink from 'components/ButtonLink';
-import Icon from 'components/Icon';
 
 // Theme variables
 const site_logo = __tkr__.settings.site_logo;
 const site_title = __tkr__.settings.meta.title;
 
-const scrollToTop = () => {
-	window.scroll({
-		top: 0,
-		behavior: 'smooth'
-	});
-};
-
 const Header = () => {
-	const [scrolled, setScrolled] = useState(false);
-	const [expanded, setExpanded] = useState(false);
-	const top_nav = useRef(null);
-	const scroll_button = useRef(null);
-	
 	const main_menu = useMainMenu();
-	
-	let header_class = 'tkr-header';
-	let mobile_drawer_btn_icon = 'menu';
-	
-	const onScroll = () => {		
-		const top_nav_height = top_nav.current.getBoundingClientRect().height;
-
-		if (pageYOffset > (top_nav_height * 0.25)) {
-			setScrolled(true);
-		} else {
-			setScrolled(false);
-		}
-	};
-
-	// Handle on scroll
-	useEffect(() => {
-		window.addEventListener('scroll', onScroll);
-
-		return () => {
-			window.removeEventListener('scroll', onScroll);
-		};
-	});
-	
-	if (scrolled) {
-		header_class = `${header_class} scrolled`;
-	}
-	
-	if (expanded) {
-		header_class = `${header_class} expanded`;
-		mobile_drawer_btn_icon = 'x';
-	}	
 
 	return(
-		<header className={ header_class } ref={ top_nav }>
-			
-			<div className="container d-flex justify-content-between">
-				
-				<Button className="tkr-header-btn tkr-mobile-drawer-btn" icon={mobile_drawer_btn_icon} onClick={ () => setExpanded(!expanded) }/>
-					
-				<A href="/" className="tkr-site-branding">
-					{ site_logo &&
-						<span className="tkr-site-logo">
-							<img src={ site_logo } />
-						</span>
-					}
-
-					{ site_title &&
-						<span className="tkr-site-title">
-							<strong>{ site_title }</strong>
-						</span>
-					}			
-				</A>
-
+		<header id="top-nav" className="tkr-header">
+			<div className="container d-flex justify-content-between align-items-center">
 				
 				<nav className="tkr-main-menu align-right">
 					<ul className="list-style-none">
@@ -97,11 +34,6 @@ const Header = () => {
 					</ul>
 				</nav>
 
-				<Button className="tkr-header-btn tkr-action-btn" icon="phone"/>
-					
-			</div>
-			
-			{/* <div className="tkr-mobile-drawer">
 				<A href="/" className="tkr-site-branding">
 					{ site_logo &&
 						<span className="tkr-site-logo">
@@ -109,34 +41,14 @@ const Header = () => {
 						</span>
 					}
 
-					{ !site_logo && site_title &&
+					{ site_title &&
 						<span className="tkr-site-title">
 							<strong>{ site_title }</strong>
 						</span>
 					}			
 				</A>
-
-				<nav className="tkr-main-menu">
-					<ul>
-						{ main_menu && main_menu.items && main_menu.items.filter(item => item.status === 'publish').map((item, index) => (
-							<li key={ index }>													
-								<ButtonLink href={ item.url.path }>
-									{ item.title }
-								</ButtonLink>
-							</li>
-						)) }
-					</ul>
-				</nav>
-			</div> */}
-
-			<span 
-				ref={ scroll_button }
-				className={`tkr-scroll-arrow ${ scrolled ? 'visible' : '' }`} 
-				onClick={ scrollToTop }
-			>
-				<Icon icon="arrow-up"/>
-			</span>
-
+					
+			</div>
 		</header>
 	);
 };
