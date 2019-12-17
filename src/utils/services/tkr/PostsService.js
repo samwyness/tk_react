9a6561c94a5e6af09c161c8b74d0024c19d98e17
Sam Wyness <samwyness22@gmail.com>
2019-12-17 17:21:57 +1000

    Added api services.

---
+++ src/utils/services/tkr/PostsService.js
@@ -0,0 +1,38 @@
+import ApiService from 'utils/services/ApiService';
+
+const { tkr_api: TKR_API } = __tkr__.urls;
+
+const PostsService = new ApiService(TKR_API);
+
+class Posts {
+	constructor() {
+		this.endpoint = 'posts';
+	}
+
+	getAll() {
+		return PostsService.send(this.endpoint);
+	}
+
+	getById(id) {
+		return PostsService.send(`${this.endpoint}/id/${id}`);
+	}
+
+	getBySlug(slug) {
+		return PostsService.send(`${this.endpoint}/${slug}`);
+	}
+
+	getSingle(options) {
+		const { id, slug } = options;
+
+		// Prefer id over slug
+		if (id) {
+			return this.getById(id);
+		} else if (slug) {
+			return this.getBySlug(slug);
+		}
+
+		return null;
+	}
+}
+
+export default Posts;
