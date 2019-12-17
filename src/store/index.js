8757f798055f828834e4576b5545c6adde2d55a3
Sam Wyness <samwyness22@gmail.com>
2019-12-17 17:23:37 +1000

    Refactor store.

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ const initialState = {
 	app: appInitialState
 };
 
-const StoreContext = React.createContext();
+const StoreContext = React.createContext(initialState);
 
 const reducer = ({ app }, action) => ({
 	app: appReducer(app, action),
@@ -13,24 +13,19 @@ const reducer = ({ app }, action) => ({
 
 /**
  * A React component that provides a store to the application
- * 
+ *
  * @param {Object} props - The React component props (used to render child components)
  */
-export const StoreProvider = ({ children }) => {
+const StoreProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState);
-	
+
 	return (
 		<StoreContext.Provider value={{ state, dispatch }}>
 			{ children }
 		</StoreContext.Provider>
 	);
 };
-  
-/**
- * useStore will be used in React components to fetch and mutate state
- */
-export const useStore = () => {
-	const { state, dispatch } = useContext(StoreContext);
-	return { state, dispatch };
-};
+
+export const useStore = () => useContext(StoreContext);
+export default StoreProvider;
 
