537a3f6b90c99d0436846ebeb1bfddc90bb08114
Sam Wyness <samwyness22@gmail.com>
2020-03-14 10:52:12 +1000

    Format store.

--- src/store/actions/appActions.js
+++ src/store/actions/appActions.js
@@ -8,28 +8,42 @@ import PostsService from 'utils/services/tkr/PostsService';
 
 const Posts = new PostsService();
 
+// App loading class
+const app_loading_class = 'tkr-app-loading';
+
+// App loading helper function
+const setAppLoadingClass = bool => {
+    if (bool) {
+        document.body.classList.add(app_loading_class);
+    } else {
+        document.body.classList.remove(app_loading_class);
+    }
+};
+
 /*-----------------------------
     POSTS
 -----------------------------*/
 const fetchPostsLoading = (payload, dispatch) => {
-	return dispatch({
-		type: 'FETCH_POSTS_LOADING',
-		payload: payload
-	});
+    setAppLoadingClass(payload);
+
+    return dispatch({
+        type: 'FETCH_POSTS_LOADING',
+        payload: payload
+    });
 };
 
 const fetchPostsSuccess = (payload, dispatch) => {
-	return dispatch({
-		type: 'FETCH_POSTS_SUCCESS',
-		payload: payload
-	});
+    return dispatch({
+        type: 'FETCH_POSTS_SUCCESS',
+        payload: payload
+    });
 };
 
 const fetchPostsError = (payload, dispatch) => {
-	return dispatch({
-		type: 'FETCH_POSTS_ERROR',
-		payload: payload
-	});
+    return dispatch({
+        type: 'FETCH_POSTS_ERROR',
+        payload: payload
+    });
 };
 
 /**
@@ -40,29 +54,29 @@ const fetchPostsError = (payload, dispatch) => {
  * @version 1.0.0
  */
 export const useFetchPosts = () => {
-	const { state, dispatch } = useStore();
-	const { posts } = state.app;
+    const { state, dispatch } = useStore();
+    const { posts } = state.app;
 
-	useEffect(() => {
-		const fetchPosts = async () => {
-			fetchPostsLoading(true, dispatch);
+    useEffect(() => {
+        const fetchPosts = async () => {
+            fetchPostsLoading(true, dispatch);
 
-			const response = await Posts.getAll();
-			const posts = await response.json();
+            const response = await Posts.getAll();
+            const posts = await response.json();
 
-			if (!response.ok) {
-				fetchPostsError(true, dispatch);
-			}
+            if (!response.ok) {
+                fetchPostsError(true, dispatch);
+            }
 
-			fetchPostsSuccess(posts, dispatch);
-			fetchPostsLoading(false, dispatch);
-		};
+            fetchPostsSuccess(posts, dispatch);
+            fetchPostsLoading(false, dispatch);
+        };
 
-		// Only fetch posts data if our stores posts.data state is empty
-		if (posts.data.length < 1) {
-			!posts.loading && fetchPosts();
-		}
-	}, [dispatch, posts.data.length, posts.loading]);
+        // Only fetch posts data if our stores posts.data state is empty
+        if (posts.data.length < 1) {
+            !posts.loading && fetchPosts();
+        }
+    }, [dispatch, posts.data.length, posts.loading]);
 };
 
 /**
@@ -72,52 +86,54 @@ export const useFetchPosts = () => {
  * @return {Array} A collection of posts
  */
 export const usePosts = () => {
-	const { state } = useStore();
-	const [posts, setPosts] = useState(state.app.posts);
+    const { state } = useStore();
+    const [posts, setPosts] = useState(state.app.posts);
 
-	useFetchPosts();
+    useFetchPosts();
 
-	useEffect(() => {
-		return setPosts(state.app.posts);
-	}, [state.app.posts]);
+    useEffect(() => {
+        return setPosts(state.app.posts);
+    }, [state.app.posts]);
 
-	return posts;
+    return posts;
 };
 
 /*-----------------------------
     SINGLE POST/PAGE
 -----------------------------*/
 const addCachedPage = (payload, dispatch) => {
-	return dispatch({
-		type: 'ADD_CACHED_PAGE',
-		payload: payload
-	});
+    return dispatch({
+        type: 'ADD_CACHED_PAGE',
+        payload: payload
+    });
 };
 
 const fetchCurrentPageLoading = (payload, dispatch) => {
-	return dispatch({
-		type: 'FETCH_CURRENT_PAGE_LOADING',
-		payload: payload
-	});
+    setAppLoadingClass(payload);
+
+    return dispatch({
+        type: 'FETCH_CURRENT_PAGE_LOADING',
+        payload: payload
+    });
 };
 
 const fetchCurrentPageSuccess = (payload, dispatch) => {
-	// Handle page caching in the store
-	if (payload && payload.post_type === 'page') {
-		addCachedPage(payload, dispatch);
-	}
+    // Handle page caching in the store
+    if (payload && payload.post_type === 'page') {
+        addCachedPage(payload, dispatch);
+    }
 
-	return dispatch({
-		type: 'FETCH_CURRENT_PAGE_SUCCESS',
-		payload: payload
-	});
+    return dispatch({
+        type: 'FETCH_CURRENT_PAGE_SUCCESS',
+        payload: payload
+    });
 };
 
 const fetchCurrentPageError = (payload, dispatch) => {
-	return dispatch({
-		type: 'FETCH_CURRENT_PAGE_ERROR',
-		payload: payload
-	});
+    return dispatch({
+        type: 'FETCH_CURRENT_PAGE_ERROR',
+        payload: payload
+    });
 };
 
 /**
@@ -127,43 +143,43 @@ const fetchCurrentPageError = (payload, dispatch) => {
  * @version 1.0.0
  * @param {String} slug - The slug for the current page
  */
-export const useFetchCurrentPage = async (slug) => {
-	const { state, dispatch } = useStore();
-	const { current_page, posts, pages } = state.app;
+export const useFetchCurrentPage = async slug => {
+    const { state, dispatch } = useStore();
+    const { current_page, posts, pages } = state.app;
 
-	useEffect(() => {
-		// Gets the current post data from the TKR Posts Service or
-		// from a cached version saved in the store.
-		const fetchPost = async () => {
-			const cached_post = posts.data.find(post => post.slug === slug);
-			const cached_page = pages.data.find(post => post.slug === slug);
+    useEffect(() => {
+        // Gets the current post data from the TKR Posts Service or
+        // from a cached version saved in the store.
+        const fetchPost = async () => {
+            const cached_post = posts.data.find(post => post.slug === slug);
+            const cached_page = pages.data.find(post => post.slug === slug);
 
-			if (cached_post) {
-				fetchCurrentPageSuccess(cached_post, dispatch);
-			} else if (cached_page) {
-				fetchCurrentPageSuccess(cached_page, dispatch);
-			} else {
-				fetchCurrentPageLoading(true, dispatch);
+            if (cached_post) {
+                fetchCurrentPageSuccess(cached_post, dispatch);
+            } else if (cached_page) {
+                fetchCurrentPageSuccess(cached_page, dispatch);
+            } else {
+                fetchCurrentPageLoading(true, dispatch);
 
-				const response = await Posts.getSingle({slug});
-				const post = await response.json();
+                const response = await Posts.getSingle({ slug });
+                const post = await response.json();
 
-				if (!response.ok) {
-					fetchCurrentPageError(true, dispatch);
-				}
+                if (!response.ok) {
+                    fetchCurrentPageError(true, dispatch);
+                }
 
-				fetchCurrentPageSuccess(post, dispatch);
-			}
+                fetchCurrentPageSuccess(post, dispatch);
+            }
 
-			fetchCurrentPageLoading(false, dispatch);
-		};
+            fetchCurrentPageLoading(false, dispatch);
+        };
 
-		// Only fetch new post data if our stores current_page
-		// is null or we have a new slug
-		if (!current_page.data || current_page.data.slug !== slug) {
-			!current_page.loading && fetchPost(slug);
-		}
-	});
+        // Only fetch new post data if our stores current_page
+        // is null or we have a new slug
+        if (!current_page.data || current_page.data.slug !== slug) {
+            !current_page.loading && fetchPost(slug);
+        }
+    });
 };
 
 /**
@@ -172,21 +188,21 @@ export const useFetchCurrentPage = async (slug) => {
  * @version 1.0.0
  * @return {Object} An object representing the current page's data
  */
-export const useCurrentPage = (slug) => {
-	const { state, dispatch } = useStore();
-	const [current_page, setCurrentPage] = useState(state.app.current_page);
+export const useCurrentPage = slug => {
+    const { state, dispatch } = useStore();
+    const [current_page, setCurrentPage] = useState(state.app.current_page);
 
-	// If we have a new slug set current page in the store to null
-	// and set the loading flag to true
-	if (current_page.data && current_page.data.slug !== slug) {
-		fetchCurrentPageSuccess(null, dispatch);
-	}
+    // If we have a new slug set current page in the store to null
+    // and set the loading flag to true
+    if (current_page.data && current_page.data.slug !== slug) {
+        fetchCurrentPageSuccess(null, dispatch);
+    }
 
-	useFetchCurrentPage(slug);
+    useFetchCurrentPage(slug);
 
-	useEffect(() => {
-		setCurrentPage(state.app.current_page);
-	}, [current_page.data, dispatch, slug, state.app.current_page]);
+    useEffect(() => {
+        setCurrentPage(state.app.current_page);
+    }, [current_page.data, dispatch, slug, state.app.current_page]);
 
-	return current_page;
+    return current_page;
 };
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,13 @@ import React, { useReducer, useContext } from 'react';
 import appReducer, { appInitialState } from 'store/reducers/appReducers';
 
 const initialState = {
-	app: appInitialState
+    app: appInitialState
 };
 
 const StoreContext = React.createContext(initialState);
 
 const reducer = ({ app }, action) => ({
-	app: appReducer(app, action),
+    app: appReducer(app, action)
 });
 
 /**
@@ -17,15 +17,14 @@ const reducer = ({ app }, action) => ({
  * @param {Object} props - The React component props (used to render child components)
  */
 const StoreProvider = ({ children }) => {
-	const [state, dispatch] = useReducer(reducer, initialState);
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-	return (
-		<StoreContext.Provider value={{ state, dispatch }}>
-			{ children }
-		</StoreContext.Provider>
-	);
+    return (
+        <StoreContext.Provider value={{ state, dispatch }}>
+            {children}
+        </StoreContext.Provider>
+    );
 };
 
 export const useStore = () => useContext(StoreContext);
 export default StoreProvider;
-
