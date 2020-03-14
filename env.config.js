43d17156dd4d2f552f2c22fdb216d4163f61c4db
Sam Wyness <samwyness22@gmail.com>
2020-03-14 10:48:13 +1000

    Format env configs.

--- env.config.js
+++ env.config.js
@@ -1,15 +1,15 @@
 const path = require('path');
 
 module.exports = {
-	paths: {
-		root: path.resolve(__dirname),
-		dist: path.resolve(__dirname, 'dist'),
-		src: path.resolve(__dirname, 'src'),
-		assets: path.resolve(__dirname, 'src/assets'),
-		css: path.resolve(__dirname, 'src/assets/css'),
-		components: path.resolve(__dirname, 'src/components'),
-		containers: path.resolve(__dirname, 'src/containers'),
-		store: path.resolve(__dirname, 'src/store'),
-		utils: path.resolve(__dirname, 'src/utils'),
-	}
+    paths: {
+        root: path.resolve(__dirname),
+        dist: path.resolve(__dirname, 'dist'),
+        src: path.resolve(__dirname, 'src'),
+        assets: path.resolve(__dirname, 'src/assets'),
+        css: path.resolve(__dirname, 'src/assets/css'),
+        components: path.resolve(__dirname, 'src/components'),
+        containers: path.resolve(__dirname, 'src/containers'),
+        store: path.resolve(__dirname, 'src/store'),
+        utils: path.resolve(__dirname, 'src/utils')
+    }
 };
