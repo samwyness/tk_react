037c1982900bf08900b845bc624f053596ab9bd4
Sam Wyness <samwyness22@gmail.com>
2020-03-14 10:46:44 +1000

    Format config files.

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const SpriteLoaderPlugin = require( 'svg-sprite-loader/plugin' );
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 
 // Theme Environment
 const THEME_ENV = require('./env.config.js');
@@ -11,99 +11,95 @@ const SRC = THEME_ENV.paths.src;
 const ASSETS = THEME_ENV.paths.assets;
 const CSS = THEME_ENV.paths.css;
 
-// Common webpack config 
+// Common webpack config
 const config = {
-	entry: {
-		bundle: path.resolve(SRC, 'index.js'),
-		style: path.resolve(CSS, 'style.scss'),
-	},
-	
-	stats: true,
+    entry: {
+        bundle: path.resolve(SRC, 'index.js'),
+        style: path.resolve(CSS, 'style.scss')
+    },
 
-	output: {
-		filename: 'tkr-[name].js',
-		publicPath: '/'
-	},
+    stats: true,
 
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			},
-			{
-				test: /\.(scss|css)$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
-					'sass-loader'
-				]
-			},
-			{
-				test: /\.(png|jpg|gif)$/,
-				use: [
-					{
-						loader: 'url-loader',
-					}
-				]
-			},
-			{
-				test: /\.svg$/,
-				include: path.resolve(ASSETS, 'icons'),
-				loader: 'svg-sprite-loader',
-				options: {
-					extract: true,
-					spriteFilename: 'icon-sprite.svg'
-				}
-			}
-		],
-	},
+    output: {
+        filename: 'tkr-[name].js',
+        publicPath: '/'
+    },
 
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				commons: {
-					test: /[\\/]node_modules[\\/]/,
-					name: 'vendors',
-					chunks: 'all'
-				},
-				styles: {
-					test: /\.css$/,
-					name: 'style',
-					chunks: 'all',
-					enforce: true
-				}
-			}
-		}
-	},
-	
-	plugins: [
-		new CleanWebpackPlugin(),
-		
-		new MiniCssExtractPlugin({
-			filename: 'tkr-[name].css',
-		}),
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.svg$/,
+                include: path.resolve(ASSETS, 'icons'),
+                loader: 'svg-sprite-loader',
+                options: {
+                    extract: true,
+                    spriteFilename: 'icon-sprite.svg'
+                }
+            }
+        ]
+    },
 
-		new SpriteLoaderPlugin( {
-			plainSprite: true,
-			spriteAttrs: {
-				id: 'tkr-svg-sprite'
-			}
-		} )
-	],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                },
+                styles: {
+                    test: /\.css$/,
+                    name: 'style',
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        }
+    },
 
-	resolve: {
-		extensions: ['.js', '.jsx'],
-		alias: {
-			assets: ASSETS,
-			css: CSS,
-			store: path.resolve(SRC, 'store'),
-			utils: path.resolve(SRC, 'utils'),
-			components: path.resolve(SRC, 'components'),
-			containers: path.resolve(SRC, 'containers'),
-		}
-	}
+    plugins: [
+        new CleanWebpackPlugin(),
+
+        new MiniCssExtractPlugin({
+            filename: 'tkr-[name].css'
+        }),
+
+        new SpriteLoaderPlugin({
+            plainSprite: true,
+            spriteAttrs: {
+                id: 'tkr-svg-sprite'
+            }
+        })
+    ],
+
+    resolve: {
+        extensions: ['ts', 'tsx', '.js', '.jsx'],
+        alias: {
+            assets: ASSETS,
+            css: CSS,
+            store: path.resolve(SRC, 'store'),
+            utils: path.resolve(SRC, 'utils'),
+            components: path.resolve(SRC, 'components'),
+            containers: path.resolve(SRC, 'containers')
+        }
+    }
 };
 
 module.exports = config;
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,13 @@
 const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const common = require('config/webpack.common.js');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const config = {
-	mode: 'production',
-	optimization: {
-		minimizer: [
-			new TerserJSPlugin({}),
-			new OptimizeCSSAssetsPlugin({})
-		],
-	},
+    mode: 'production',
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    }
 };
 
 module.exports = merge(common, config);
